735ec0263a425c1e5e201ec625c6896300700abf
yuhaochua <yuhaochua99@gmail.com>
2020-04-22 12:20:54 +0800

    Creating subject flashcards

--- src/App.js
+++ src/App.js
@@ -15,6 +15,35 @@ class App extends Component{
       return (
           <div className='App pt-4'>  
               <h1 className="d-flex justify-content-center">Revision Flashcards</h1>
+
+              <div className="container mt-3 d-flex justify-content-around">
+                <div className="card" style={{width: 18 + 'rem'}}>
+                  <img src="chemistry.png" className="card-img-top py-4 px-3" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Chemistry</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button className="btn btn-primary">See cards</button>
+                  </div>
+                </div>
+
+                <div className="card" style={{width: 18 + 'rem'}}>
+                  <img src="calculus.png" className="card-img-top py-4 px-3" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Math</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button className="btn btn-primary">See cards</button>
+                  </div>
+                </div>
+
+                <div className="card" style={{width: 18 + 'rem'}}>
+                  <img src="world.png" className="card-img-top py-4 px-3" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Geography</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button className="btn btn-primary">See cards</button>
+                  </div>
+                </div>
+              </div>
           </div>
       );
   }
